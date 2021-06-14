e261fb5cf5ff5d7a6051a1a04b55ec95b412f962
Aziz Patel <ac98844@avcol.school.nz>
2021-06-14 14:14:37 +1200

    new people added

---
+++ Migrations/20210614012509_School.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AvondaleTyres.Migrations
+{
+    public partial class School : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
