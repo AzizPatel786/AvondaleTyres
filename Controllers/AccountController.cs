083cf7c4ddfb04b1e4c9b1871106a8faf3a9958c
Aziz Patel <ac98844@avcol.school.nz>
2021-06-17 18:47:29 +1200

    Login working after removing double post action

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -73,24 +73,24 @@ namespace AvondaleTyres.Controllers
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var result = await signInManager.PasswordSignInAsync(
-                    model.Email, model.Password, model.RememberMe, false);
+        //[HttpPost]
+        //public async Task<IActionResult> Login(LoginViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var result = await signInManager.PasswordSignInAsync(
+        //            model.Email, model.Password, model.RememberMe, false);
 
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("index", "home");
-                }
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToAction("index", "home");
+        //        }
 
-                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
-            }
+        //        ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+        //    }
 
-            return View(model);
-        }
+        //    return View(model);
+        //}
         [HttpPost]
         [AllowAnonymous]
         public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
